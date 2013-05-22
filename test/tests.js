c8bf78f696e3f5e452b5b2119333d8efe1aa7682
Peter Simon <simonpepe@gmail.com>
2013-05-22 12:28:57 +0200

    Refactoring tests

--- test/tests.js
+++ test/tests.js
@@ -13,6 +13,11 @@
     $city2.val('');
   };
 
+  var setValueAndLeaveTheField = function($node, value) {
+    $node.val(value).trigger('blur');
+  };
+
+
   $(function() {
     $zip = $('#zip');
     $city = $('#city');
@@ -26,94 +31,94 @@
 
     // Iranyitoszambol varos
     q.test('Hibas iranyitoszam (nem szam)', function() {
-      $zip.val('abcd').trigger('blur');
+      setValueAndLeaveTheField($zip, 'abcd');
 
       q.equal($city.val(), '');
     });
 
     q.test('Hibas iranyitoszam (tul hosszu)', function() {
-      $zip.val('10410').trigger('blur');
+      setValueAndLeaveTheField($zip, '10410');
 
       q.equal($city.val(), '');
     });
 
     q.test('Hibas iranyitoszam (hibas forma)', function() {
-      $zip.val('0123').trigger('blur');
+      setValueAndLeaveTheField($zip, '0123');
 
       q.equal($city.val(), '');
     });
 
     q.test('Feher szokozok levagasa', function() {
-      $zip.val('	7632  ').trigger('blur');
+      setValueAndLeaveTheField($zip, '	7632  ');
 
       q.equal($city.val(), 'Pécs');
     });
 
     q.test('Varos mar ki van toltve', function() {
       $city.val('Budapest');
-      $zip.val('7632').trigger('blur');
+      setValueAndLeaveTheField($zip, '7632');
 
       q.equal($city.val(), 'Budapest');
     });
 
     q.test('Nem egyertelmu a varos', function() {
-      $zip.val('7915').trigger('blur');
+      setValueAndLeaveTheField($zip, '7915');
 
       q.equal($city.val(), '');
     });
 
     q.test('Varos kikeresese', function() {
-      $zip.val('7632').trigger('blur');
+      setValueAndLeaveTheField($zip, '7632');
 
       q.equal($city.val(), 'Pécs');
     });
 
     q.test('Varos kikeresese (masodik peldany)', function() {
-      $zip2.val('7632').trigger('blur');
+      setValueAndLeaveTheField($zip2, '7632');
 
       q.equal($city2.val(), 'Pécs');
     });
 
     // Varosbol iranyitoszam
     q.test('Nincs ilyen varos', function() {
-      $city.val('abcd').trigger('blur');
+      setValueAndLeaveTheField($city, 'abcd');
 
       q.equal($zip.val(), '');
     });
 
     q.test('Feher szokozok levagasa', function() {
-      $city.val('	Nagyatád  ').trigger('blur');
+      setValueAndLeaveTheField($city, '	Nagyatád  ');
 
       q.equal($zip.val(), '7500');
     });
 
     q.test('kis- es nagybetu kozott nincs kulonbseg', function() {
-      $city.val('naGyAtÁd').trigger('blur');
+      setValueAndLeaveTheField($city, 'naGyAtÁd');
 
       q.equal($zip.val(), '7500');
     });
 
     q.test('Iranyitoszam mar ki van toltve', function() {
       $zip.val('7632');
-      $city.val('Nagyatád').trigger('blur');
+      setValueAndLeaveTheField($city, 'Nagyatád');
 
       q.equal($zip.val(), '7632');
     });
 
     q.test('Nem egyertelmu az iranyitoszam', function() {
-      $city.val('Budapest').trigger('blur');
+      setValueAndLeaveTheField($city, 'Budapest');
 
       q.equal($zip.val(), '');
     });
 
     q.test('Iranyitoszam kikeresese', function() {
-      $city.val('Nagyatád').trigger('blur');
+      setValueAndLeaveTheField($city, 'Nagyatád');
 
       q.equal($zip.val(), '7500');
     });
 
     q.test('Iranyitoszam kikeresese (masodik peldany)', function() {
-      $city2.val('Nagyatád').trigger('blur');
+      setValueAndLeaveTheField($city2, 'Nagyatád');
 
       q.equal($zip2.val(), '7500');
     });
